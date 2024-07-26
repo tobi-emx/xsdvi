a51cbfa397f1ed4bbaa4e9f245d470523d4c5e92
tobias.columbus <tobias.columbus@emmtrix.com>
2024-07-26 14:09:20 +0200

    formatting

--- src/main/java/xsdvi/svg/SymbolElement.java
+++ src/main/java/xsdvi/svg/SymbolElement.java
@@ -1,177 +1,176 @@
 package xsdvi.svg;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.commons.text.WordUtils;
 import xsdvi.utils.WidthCalculator;
 
 /**
  * @author Václav Slavìtínský
  *
  */
-public class SymbolElement extends AbstractSymbol{	
-	private String name = null;
-	private String namespace = null;
-	private String type = null;
-	private String cardinality = null;
-        private boolean optional = false;
-	private boolean nillable = false;
-	private boolean abstr = false;
-	private String substitution = null;
-	
-	/**
-	 * @param name
-	 * @param namespace
-	 * @param type
-	 * @param cardinality
-	 * @param nillable
-	 * @param abstr
-	 * @param substitution
-	 */
-	public SymbolElement(String name, String namespace, String type, String cardinality, boolean nillable, boolean abstr, String substitution) {
-		this();
-		this.name = name;
-		this.namespace = namespace;
-		this.type = type;
-		this.cardinality = cardinality;
-		this.nillable = nillable;
-		this.abstr = abstr;
-		this.substitution = substitution;
-	}
+public class SymbolElement extends AbstractSymbol {
 
-	/**
-	 * 
-	 */
-	public SymbolElement() {
-		super();
-	}
-	
-	/**
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
+    private String name = null;
+    private String namespace = null;
+    private String type = null;
+    private String cardinality = null;
+    private boolean optional = false;
+    private boolean nillable = false;
+    private boolean abstr = false;
+    private String substitution = null;
 
-	/**
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * @param name
+     * @param namespace
+     * @param type
+     * @param cardinality
+     * @param nillable
+     * @param abstr
+     * @param substitution
+     */
+    public SymbolElement(String name, String namespace, String type, String cardinality, boolean nillable, boolean abstr, String substitution) {
+        this();
+        this.name = name;
+        this.namespace = namespace;
+        this.type = type;
+        this.cardinality = cardinality;
+        this.nillable = nillable;
+        this.abstr = abstr;
+        this.substitution = substitution;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getNamespace() {
-		return namespace;
-	}
+    /**
+     *
+     */
+    public SymbolElement() {
+        super();
+    }
 
-	/**
-	 * @param namespace
-	 */
-	public void setNamespace(String namespace) {
-		this.namespace = namespace;
-	}
+    /**
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getType() {
-		return type;
-	}
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * @param type
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
+    /**
+     * @return
+     */
+    public String getNamespace() {
+        return namespace;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getCardinality() {
-		return cardinality;
-	}
+    /**
+     * @param namespace
+     */
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
 
-	/**
-	 * @param cardinality
-	 */
-	public void setCardinality(String cardinality) {
-		this.cardinality = cardinality;
-                if (cardinality != null && cardinality.startsWith("0")) {
-                    this.optional = true;
-                }
-	}
+    /**
+     * @return
+     */
+    public String getType() {
+        return type;
+    }
 
-	/**
-	 * @return
-	 */
-	public boolean isNillable() {
-		return nillable;
-	}
+    /**
+     * @param type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	/**
-	 * @param nillable
-	 */
-	public void setNillable(boolean nillable) {
-		this.nillable = nillable;
-	}
+    /**
+     * @return
+     */
+    public String getCardinality() {
+        return cardinality;
+    }
 
-	/**
-	 * @return
-	 */
-	public boolean isAbstr() {
-		return abstr;
-	}
+    /**
+     * @param cardinality
+     */
+    public void setCardinality(String cardinality) {
+        this.cardinality = cardinality;
+        if (cardinality != null && cardinality.startsWith("0")) {
+            this.optional = true;
+        }
+    }
 
-	/**
-	 * @param abstr
-	 */
-	public void setAbstr(boolean abstr) {
-		this.abstr = abstr;
-	}
+    /**
+     * @return
+     */
+    public boolean isNillable() {
+        return nillable;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getSubstitution() {
-		return substitution;
-	}
+    /**
+     * @param nillable
+     */
+    public void setNillable(boolean nillable) {
+        this.nillable = nillable;
+    }
 
-	/**
-	 * @param substitution
-	 */
-	public void setSubstitution(String substitution) {
-		this.substitution = substitution;
-	}
+    /**
+     * @return
+     */
+    public boolean isAbstr() {
+        return abstr;
+    }
+
+    /**
+     * @param abstr
+     */
+    public void setAbstr(boolean abstr) {
+        this.abstr = abstr;
+    }
+
+    /**
+     * @return
+     */
+    public String getSubstitution() {
+        return substitution;
+    }
+
+    /**
+     * @param substitution
+     */
+    public void setSubstitution(String substitution) {
+        this.substitution = substitution;
+    }
 
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see xsdvi.svg.AbstractSymbol#draw()
-	 */
-	@Override
-	public void draw() {
-		print("<a href=\"#\" onclick=\"window.parent.location.href = window.parent.location.href.split('#')[0]  + '#element_" +name+ "'\">");
+     */
+    @Override
+    public void draw() {
+        print("<a href=\"#\" onclick=\"window.parent.location.href = window.parent.location.href.split('#')[0]  + '#element_" + name + "'\">");
 
-		processDescription();
+        processDescription();
 
-		drawGStart();
-		print("<rect class='shadow' x='3' y='3' width='"+width+"' height='"+height+"'/>");
-                if (optional) {
-                    print("<rect class='boxelementoptional' x='0' y='0' width='"+width+"' height='"+height+"'");
-                } else {
-                    print("<rect class='boxelement' x='0' y='0' width='"+width+"' height='"+height+"'");
-                }
-                // all items display, no need to mouseover event
-		//drawMouseover();
-                print("/>");
-		if (namespace!=null) {
-			print("<text class='visible' x='5' y='13'>"+namespace+"</text>");
-		}
-		/*if (substitution!=null) {
+        drawGStart();
+        print("<rect class='shadow' x='3' y='3' width='" + width + "' height='" + height + "'/>");
+        if (optional) {
+            print("<rect class='boxelementoptional' x='0' y='0' width='" + width + "' height='" + height + "'");
+        } else {
+            print("<rect class='boxelement' x='0' y='0' width='" + width + "' height='" + height + "'");
+        }
+        // all items display, no need to mouseover event
+        //drawMouseover();
+        print("/>");
+        if (namespace != null) {
+            print("<text class='visible' x='5' y='13'>" + namespace + "</text>");
+        }
+        /*if (substitution!=null) {
 			print("<text class='hidden' visibility='hidden' x='5' y='13'>subst.: "+substitution+"</text>");
 			print("<text class='hidden' visibility='hidden' x='5' y='41'>nillable: "+(nillable ? "1" : "0")+", abstract: "+(abstr ? "1" : "0")+"</text>");
 		}
@@ -179,90 +178,89 @@ public class SymbolElement extends AbstractSymbol{
 			print("<text class='hidden' visibility='hidden' x='5' y='13'>nillable: "+(nillable ? "1" : "0")+"</text>");
 			print("<text class='hidden' visibility='hidden' x='5' y='41'>abstract: "+(abstr ? "1" : "0")+"</text>");
 		}*/
-		if (name!=null) {
-			print("<text class='strong elementlink' x='5' y='27'>"+name+"</text>");
-		}
-		if (type!=null) {
-			print("<text class='visible' x='5' y='41'>"+type+"</text>");
-		}
-                
-                ArrayList<String> propertiesArray = new ArrayList<>();
-		if (cardinality!=null) {
-			propertiesArray.add(cardinality);
-		}
-		if (substitution!=null) {
-			propertiesArray.add("subst.: "+substitution);
-		}
-		if (nillable) {
-			propertiesArray.add("nillable: true");
-		}
-		if (abstr) {
-			propertiesArray.add("abstract: true");
-		}
-		String properties = String.join(", ", propertiesArray);
-		print("<text x='5' y='59'>"+properties+"</text>");
+        if (name != null) {
+            print("<text class='strong elementlink' x='5' y='27'>" + name + "</text>");
+        }
+        if (type != null) {
+            print("<text class='visible' x='5' y='41'>" + type + "</text>");
+        }
 
-		drawDescription(59);
+        ArrayList<String> propertiesArray = new ArrayList<>();
+        if (cardinality != null) {
+            propertiesArray.add(cardinality);
+        }
+        if (substitution != null) {
+            propertiesArray.add("subst.: " + substitution);
+        }
+        if (nillable) {
+            propertiesArray.add("nillable: true");
+        }
+        if (abstr) {
+            propertiesArray.add("abstract: true");
+        }
+        String properties = String.join(", ", propertiesArray);
+        print("<text x='5' y='59'>" + properties + "</text>");
 
-		drawConnection();
-		drawUse();
-		drawGEnd();
-                print("</a>");
-	}
-	
-	/**
-	 * 
-	 */
-	void drawAlternate() {
-		drawGStart();
-		print("<rect class='shadow' x='3' y='3' width='"+width+"' height='"+height+"'/>");
-		print("<rect class='boxelement' x='0' y='0' width='"+width+"' height='"+height+"'");
-		drawMouseover();
-		if (namespace!=null) {
-			print("<text class='hidden' visibility='hidden' x='5' y='13'>"+namespace+"</text>");
-		}
-		if (substitution!=null) {
-			print("<text class='visible' x='5' y='13'>subst.: "+substitution+"</text>");
-			print("<text class='visible' x='5' y='41'>nillable: "+(nillable ? "1" : "0")+", abstract: "+(abstr ? "1" : "0")+"</text>");
-		}
-		else {
-			print("<text class='visible' x='5' y='13'>nillable: "+(nillable ? "1" : "0")+"</text>");
-			print("<text class='visible' x='5' y='41'>abstract: "+(abstr ? "1" : "0")+"</text>");
-		}
-		if (name!=null) {
-			print("<text class='strong' x='5' y='27'>"+name+"</text>");
-		}
-		if (type!=null) {
-			print("<text class='hidden' visibility='hidden' x='5' y='41'>"+type+"</text>");
-		}
-		if (cardinality!=null) {
-			print("<text x='5' y='59'>"+cardinality+"</text>");
-		}
-		drawConnection();
-		drawUse();
-		drawGEnd();
-	}
+        drawDescription(59);
 
-	/* (non-Javadoc)
+        drawConnection();
+        drawUse();
+        drawGEnd();
+        print("</a>");
+    }
+
+    /**
+     *
+     */
+    void drawAlternate() {
+        drawGStart();
+        print("<rect class='shadow' x='3' y='3' width='" + width + "' height='" + height + "'/>");
+        print("<rect class='boxelement' x='0' y='0' width='" + width + "' height='" + height + "'");
+        drawMouseover();
+        if (namespace != null) {
+            print("<text class='hidden' visibility='hidden' x='5' y='13'>" + namespace + "</text>");
+        }
+        if (substitution != null) {
+            print("<text class='visible' x='5' y='13'>subst.: " + substitution + "</text>");
+            print("<text class='visible' x='5' y='41'>nillable: " + (nillable ? "1" : "0") + ", abstract: " + (abstr ? "1" : "0") + "</text>");
+        } else {
+            print("<text class='visible' x='5' y='13'>nillable: " + (nillable ? "1" : "0") + "</text>");
+            print("<text class='visible' x='5' y='41'>abstract: " + (abstr ? "1" : "0") + "</text>");
+        }
+        if (name != null) {
+            print("<text class='strong' x='5' y='27'>" + name + "</text>");
+        }
+        if (type != null) {
+            print("<text class='hidden' visibility='hidden' x='5' y='41'>" + type + "</text>");
+        }
+        if (cardinality != null) {
+            print("<text x='5' y='59'>" + cardinality + "</text>");
+        }
+        drawConnection();
+        drawUse();
+        drawGEnd();
+    }
+
+    /* (non-Javadoc)
 	 * @see xsdvi.svg.AbstractSymbol#getWidth()
-	 */
-	@Override
-	public int getWidth() {
-		WidthCalculator calc = new WidthCalculator(MIN_WIDTH);
-		calc.newWidth(15, name, 3);
-		calc.newWidth(15, namespace);
-		calc.newWidth(15, type);
-		calc.newWidth(15, cardinality);
-		calc.newWidth(15, (substitution==null ? 11 : 22));
-		calc.newWidth(15, substitution, 8);
-		return calc.getWidth();
-	}
-	
-	/* (non-Javadoc)
+     */
+    @Override
+    public int getWidth() {
+        WidthCalculator calc = new WidthCalculator(MIN_WIDTH);
+        calc.newWidth(15, name, 3);
+        calc.newWidth(15, namespace);
+        calc.newWidth(15, type);
+        calc.newWidth(15, cardinality);
+        calc.newWidth(15, (substitution == null ? 11 : 22));
+        calc.newWidth(15, substitution, 8);
+        return calc.getWidth();
+    }
+
+    /* (non-Javadoc)
 	 * @see xsdvi.svg.AbstractSymbol#getHeight()
-	 */
-	@Override
-	public int getHeight() {
-		return MAX_HEIGHT;
-	}
+     */
+    @Override
+    public int getHeight() {
+        return MAX_HEIGHT;
+    }
 }
